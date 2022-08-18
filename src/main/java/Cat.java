06a3739659a5ac88d48bcca5b8ae1a27aa5fc218
sezergemtsov <sezer.gemtsov@ya.ru>
2022-08-18 17:20:10 +0300

    init commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,52 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Cat {
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private int upvotes;
+
+    public Cat(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") int upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public Cat() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "____________________\n"
+                + id + "\n"
+                + text + "\n"
+                + type + "\n"
+                + user + "\n"
+                + upvotes + "\n"
+                + "___________________\n";
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpHeaders;
+
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class Main {
+
+    public static ObjectMapper oMapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+            httpGet.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+            try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+
+                List<Cat> cats = oMapper.readValue(
+                        response1.getEntity().getContent(),
+                        new TypeReference<>() {}
+                );
+                List<Cat> sortedCats = cats.stream()
+                        .filter(x->x.getUpvotes()>0)
+                        .toList();
+                sortedCats.forEach(System.out::println);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
